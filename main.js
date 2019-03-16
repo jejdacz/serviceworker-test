1c83242dbcaa19b559e02f6eb4ffb3e90cc824a9
jejdacz <markhunter@seznam.cz>
2019-03-16 09:45:22 +0100

    change absolute paths to relative

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ if ("serviceWorker" in navigator) {
   console.log("Service Worker supported");
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/serviceworker.js")
+      .register("serviceworker.js")
       .then(reg => console.log("Service Worker registered"))
       .catch(err => console.log(`Service Worker error: ${err}`));
   });
